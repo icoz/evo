bfd6e7711fd8157ad089b26e09ef3b57ab836163
icoz <icoz.vt@gmail.com>
2012-05-27 19:39:11 +0400

    Mods in mainwindow.*

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,23 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //World w;
+    QList<AnimalCommand> c;
+    c.append(start);
+    c << action_move_left << action_move_down << action_move_right
+         << action_move_down << end;
+    QList<char> cmds = Animal::compile(c);
+    QList<char> mems;
+    w.addAnimal(cmds,mems);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    w.makeStep();
+    ui->label->setPixmap(QPixmap::fromImage(w.getImage()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "world.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    World w;
 };
 
 #endif // MAINWINDOW_H
