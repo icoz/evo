2aceb836858810bca9a27f9c406546709287af4a
icoz <icoz.vt@gmail.com>
2012-10-19 16:29:04 +0400

    added qrand initialization

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    
+    qsrand(QDateTime::currentDateTimeUtc().toMSecsSinceEpoch());
     return a.exec();
 }
